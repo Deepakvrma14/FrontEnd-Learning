f53c5426c1e89b6c15dfba87bb9604b9de06bcc3
Deepakvrma14 <deepakverma1482003@gmail.com>
2023-11-19 11:37:55 +0530

    []

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -2,40 +2,88 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-function BookList(){
+
+const author = 'Jordan Moore';
+const title = 'Interesting Facts For Curious Minds';
+const img = './images/book-1.jpg';
+
+function BookList() {
   return (
-    <section>
+    <section className='booklist'>
       <Book />
-      <Book />
-      <Book />
-      
+      <Book job ='developer'/>
+      <Book title ='New Book' number = {69}/>
     </section>
   );
 }
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author} </h4>
+      <p>{props.job}</p>
+      <p> {props.title} </p>
+      <p> {props.number} </p>
+      {console.log(props)}
     </article>
   );
 };
 
-const Image = () => (
-  <img src='./images/book-1.jpg' alt='Interesting Facts For Curious Minds' />
-);
-// const Image = () => (
-//   <img
-//     src='https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg'
-//     alt='Interesting Facts For Curious Minds'
-//   />
-// );
-const Title = () => {
-  return <h2>Interesting Facts For Curious Minds</h2>;
-};
-const Author = () => <h4>Jordan Moore </h4>;
+// function BookList(){
+//   return (
+//     <section>
+//       <Book />
+//       <Book />
+//       <Book />
+      
+//     </section>
+//   );
+// }
+// const author = 'Jordan Moore';
+// const Book = () => {
+//   const title = 'Interesting Facts For Curious Mindssssss';
+//   return (
+//     <article className='book'>
+//       <img
+//         src='./images/book-1.jpg'
+//         alt='Interesting Facts For Curious Minds'
+//       />
+//       <h2>{title}</h2>
 
+//       <h4>{author.toUpperCase()} </h4>
+//       {/* <p>{let x = 6}</p> */}
+//       <p>{6 + 6}</p>
+//     </article>
+//   );
+// };
+
+// const Image = () => (
+//   <img src='./images/book-1.jpg' alt='Interesting Facts For Curious Minds' />
+// );
+// // const Image = () => (
+// //   <img
+// //     src='https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg'
+// //     alt='Interesting Facts For Curious Minds'
+// //   />
+// // );
+// const Title = () => {
+//   return <h2>Interesting Facts For Curious Minds</h2>;
+// };
+// // const Author = () => (
+// //   <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem' }}>
+// //     Jordan Moore
+// //   </h4>
+// // );
+// const Author = () => {
+//   const inlineHeadingStyles = {
+//     color: '#617d98',
+//     fontSize: '0.75rem',
+//     marginTop: '0.5rem',
+//   };
+//   return <h4 style={inlineHeadingStyles}>Jordan Moore </h4>;
+// };
 
 // const Person = () => <h2>john doe</h2>;
 // const Message = () => {
