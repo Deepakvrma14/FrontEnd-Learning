7c70bc2d7136d0b0b16bafc056f8c133090e1ad8
Deepakvrma14 <deepakverma1482003@gmail.com>
2023-12-10 23:03:57 +0530

    birtday wish project

---
+++ 04-fundamental-projects/01-birthday-buddy/starter/src/List.jsx
@@ -0,0 +1,13 @@
+import {useState} from 'react';
+
+const List = ({person}) => {
+   return (
+   <section>
+    {person.map((people) => {
+        return (
+            
+        );
+    })}
+   </section>
+   );
+};
