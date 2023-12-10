67cebec80a7f97bb083d3c8aa763bc534c06ff66
Deepakvrma14 <deepakverma1482003@gmail.com>
2023-12-10 18:35:40 +0530

    UseReducers

--- 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,57 +1,58 @@
-import React from 'react';
 import { useReducer } from 'react';
 import { data } from '../../../data';
 
-const CLEAR = 'clear_list';
-const RESET = 'reset_list';
-const REMOVE = 'remove_list';
-
+import { CLEAR_LIST, REMOVE_ITEM, RESET_LIST } from './action';
+import reducer from './reducer';
 const defaultState = {
-  people:data
-}
-
-const reducer = (state, action) => {
-  console.log(action);
-  switch(action.type){
-    case CLEAR:
-      return {...state, people:[]};
-  }
-}
+  people: data,
+};
 const ReducerBasics = () => {
-  // const [people, setPeople] = React.useState(data);
-  const [state, dispatch] = useReducer(reducer)
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const removeItem = (id) => {
-    dispatch
+    dispatch({type : REMOVE_ITEM, payload: id});
   };
-  const resetList = ()=>{
-    setPeople(data);
+
+  const clearList = () => {
+    dispatch({ type: CLEAR_LIST });
+  };
+
+  const resetList = () => {
+    dispatch({type: RESET_LIST});
   };
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
+        
         const { id, name } = person;
         return (
           <div key={id} className='item'>
+            
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
-            
           </div>
         );
       })}
-      {people.length<1 ? (
-        <button className='btn' onClick={resetList}>Reset List</button>
-      ):(
-        <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
-      >
-        clear items
-      </button>
-      )}
 
-      
+      {state.people.length < 1 ? (
+        <button
+          className='btn'
+          style={{ marginTop: '2rem' }}
+          onClick={resetList}
+        >
+          reset
+        </button>
+      ) : (
+        <button
+          className='btn'
+          style={{ marginTop: '2rem' }}
+          onClick={clearList}
+        >
+          clear
+        </button>
+
+      )}
+      <h3> usingReducers  </h3>
     </div>
   );
 };
