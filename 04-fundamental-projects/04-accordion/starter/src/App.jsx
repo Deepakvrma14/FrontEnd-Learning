2cdaf375de5c48abfe5839b9eb5ab681c68780ea
Deepakvrma14 <deepakverma1482003@gmail.com>
2023-12-12 09:52:21 +0530

    accordion completely done

--- 04-fundamental-projects/04-accordion/starter/src/App.jsx
+++ 04-fundamental-projects/04-accordion/starter/src/App.jsx
@@ -1,6 +1,22 @@
-import questions from "./data";
+import {useState} from "react";
+import question from "./data";
+import SingleQuestion from "./SingleQuestion";
 const App = () => {
-  
-
+    const [questions, setQuestions] = useState(question);
+    return(
+        <main>
+            <div className="container">
+                <section className="info">
+                    {questions.map((question) => {
+                        return(
+                            <SingleQuestion key={question.id} {...question} />
+                        );
+                    })}
+                </section>
+                
+            </div>
+        </main>
+    
+    );
 };
 export default App;
