23624c4a4497508b98b9cbf917a7ff1d4600a729
Deepakvrma14 <deepakverma1482003@gmail.com>
2023-12-12 18:11:06 +0530

    Menu Creation done

--- 04-fundamental-projects/05-menu/starter/src/App.jsx
+++ 04-fundamental-projects/05-menu/starter/src/App.jsx
@@ -1,4 +1,39 @@
-const App = () => {
-  return <h2>Menu Starter</h2>;
-};
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import Title from './Titile';
+import items from './data';
+
+const temp = new Set();
+items.forEach((item) => {
+  temp.add(item.category);
+});
+const allCategories = ['all', ...temp];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  console.log(allCategories);
+
+  return (
+    <main>
+      <section className='menu'>
+        <Title text='our menu' />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
 export default App;
