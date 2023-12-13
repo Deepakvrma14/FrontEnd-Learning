67617f58d237e9952252de630e4d14d91742a3b3
Deepakvrma14 <deepakverma1482003@gmail.com>
2023-12-13 15:06:24 +0530

    Lorem ipsum generator done

--- 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
+++ 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
@@ -1,4 +1,34 @@
+import {useState} from 'react';
+import {nanoid} from 'nanoid';
+
+import data from './data';
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text,setText] = useState([]);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let newText = data.slice(0,count);
+    setText(newText);
+  };
+
+
+  return (
+    <section  >
+      <form onSubmit={handleSubmit} >
+        <label htmlFor="amount">paragraphs: </label>
+        <input type='number' name='amount' value = {count} 
+          min='1' step='1' max='8'
+        onChange = {((e) => setCount(e.target.value))} />
+        <button type="submit">generate</button>
+      </form>
+      <article >
+        {text.map((item) => {
+          return <p key={nanoid()}>{item}</p>;
+        })}
+      </article>
+
+    </section>
+  );
 };
 export default App;
