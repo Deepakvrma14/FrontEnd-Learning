c7246ea51a75150ae106fe65852d8e30a8ed8d14
Deepakvrma14 <deepakverma1482003@gmail.com>
2023-12-13 11:34:42 +0530

    carousal done

--- 04-fundamental-projects/07-slider/starter/src/App.jsx
+++ 04-fundamental-projects/07-slider/starter/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useState } from 'react';
 import { Carousal } from './Carousal';
 const App = () => {
   return (
--- 04-fundamental-projects/07-slider/starter/src/Carousal.jsx
+++ 04-fundamental-projects/07-slider/starter/src/Carousal.jsx
@@ -1,23 +1,56 @@
-import { shortList } from "./data";
-import { useState } from "react";
+import { shortList, longList } from "./data";
+import { useState, useEffect } from "react";
 export const Carousal = () => {
-    const [people, setPeople] = useState(shortList);
+    const [people, setPeople] = useState(longList);
+    const [currentPerson, setCurrentPerson] = useState(0);
+
+    const prevSlide = () =>{
+        setCurrentPerson((oldPerson)=>{
+            const result = (oldPerson - 1 + people.length) % people.length;
+            return result;
+        })
+    };
+    const nextSlide = () =>{
+        setCurrentPerson((oldPerson)=>{
+            const result = (oldPerson+1) % people.length;
+            return result;
+        })
+    };
+
+    useEffect(()=>{
+        let slider = setInterval(()=>{
+            nextSlide();
+        
+        }, 500);
+        return ()=>{
+            clearInterval(slider);
+
+        }
+    }, [currentPerson] ) ;   
     return(
         <section className="slider-container">
-            {people.map((person)=>{
-                const {id, image, name, title, quote} = person;
-                return(
-                    <article key={id} className="slide">
-                        <img src={image} alt={name} className="person-img"/>
-                        <h4 className="name">{name}</h4>
-                        <p className="title">{title}</p>
-                        <p className="text">{quote}</p>
-                    </article>
-                );
-            })}
+            {people.map((person, personIndex) => {
+        const { id, image, name, title, quote } = person;
+        return (
+          <article
+            className='slide'
+            style={{
+              transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
+              opacity: personIndex === currentPerson ? 1 : 0,
+              visibility: personIndex === currentPerson ? 'visible' : 'hidden',
+            }}
+            key={id}
+          >
+            <img src={image} alt={name} className='person-img' />
+            <h5 className='name'>{name}</h5>
+            <p className='title'>{title}</p>
+            <p className='text'>{quote}</p>
+          </article>
+        );
+      })}
             
-        <button className="prev">- </button>
-        <button className="next">+ </button>
+        <button className="prev" onClick={prevSlide} >- </button>
+        <button className="next" onClick={nextSlide} >+ </button>
         </section>
     );
 };
