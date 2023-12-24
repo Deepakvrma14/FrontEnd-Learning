65d6e21fc664919470d9df037f91aa91da325832
Deepakvrma14 <deepakverma1482003@gmail.com>
2023-12-24 21:58:04 +0530

    learning react quary

--- 06-react-query/starter/src/App.jsx
+++ 06-react-query/starter/src/App.jsx
@@ -5,7 +5,7 @@ import Items from './Items';
 import { useState } from 'react';
 const defaultItems = [
   { id: nanoid(), title: 'walk the dog', isDone: false },
-  { id: nanoid(), title: 'wash dishes', isDone: false },
+  { id: nanoid(), title: 'wash dishes', isDone: true },
   { id: nanoid(), title: 'drink coffee', isDone: true },
   { id: nanoid(), title: 'take a nap', isDone: false },
 ];
