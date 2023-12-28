0c0d46578b16f11dcd34b0a2afce8ff40b3bcf00
Deepakvrma14 <deepakverma1482003@gmail.com>
2023-12-28 17:35:08 +0530

    react quaries

--- 06-react-query/starter/src/Items.jsx
+++ 06-react-query/starter/src/Items.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { UseQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import SingleItem from './SingleItem';
 const Items = ({ items }) => {
+  const result = useQuery({
+    queryKey: ['items'],
+    queryFn : () => customFetch.get('/'),
+    // queryFn: () => {
+    //   return fetch('http://localhost:3000/items').then((res) => res.json());
+    // },
+  });
+  console.log(result);
   return (
     <div className='items'>
+      
       {items.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
