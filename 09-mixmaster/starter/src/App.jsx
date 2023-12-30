5dc54a763c6612ba60dd171298c00d4436c9cedc
Deepakvrma14 <deepakverma1482003@gmail.com>
2023-12-30 22:59:52 +0530

    react rourters basic done

--- 09-mixmaster/starter/src/App.jsx
+++ 09-mixmaster/starter/src/App.jsx
@@ -1,4 +1,26 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+const About = () => <h1>About</h1>;
+const Users = () => <h1>Users</h1>;
+
 const App = () => {
-  return <h2>mixmaster starter</h2>;
+  return(
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Users />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
+
 export default App;
