4f5a545be2a2d5eb1f2808bdcf88ce4d0d34e9cb
Deepakvrma14 <deepakverma1482003@gmail.com>
2024-01-09 02:12:14 +0530

    done with async functionality with createAsyncThunk

--- 11-redux-toolkit-tutorial/starter/src/App.js
+++ 11-redux-toolkit-tutorial/starter/src/App.js
@@ -2,19 +2,29 @@ import { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
 import { useSelector, useDispatch } from 'react-redux';
-import { calculateTotal } from './features/cart/cartSlice';
+import { calculateTotal,getCartItems } from './features/cart/cartSlice';
 import Modal from './components/Modal';
 
 
 function App() {
-  const { cartItems } = useSelector((state) => state.cart);
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
 const { isOpen } = useSelector((state) => state.modal);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(calculateTotal());
   }, [cartItems]);
 
- 
+  useEffect(()=>{
+    dispatch(getCartItems());
+
+  },[]);
+  if (isLoading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <main>
